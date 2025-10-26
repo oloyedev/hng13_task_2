62bb1951a8134d179fcdbf1bf22f32a30f596585
Olayide Olatunji <oloyeadz@gmail.com>
2025-10-26 08:16:56 +0100

    Prepared project for Railway deployment

--- app/database.py
+++ app/database.py
@@ -7,13 +7,7 @@ from urllib.parse import quote_plus
 # Try Railway DATABASE_URL first
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if not DATABASE_URL:
-    from .config import settings
-    password = quote_plus(settings.DB_PASSWORD)
-    DATABASE_URL = (
-        f"mysql+pymysql://{settings.DB_USERNAME}:{password}"
-        f"@{settings.DB_HOST_NAME}:{settings.DB_PORT}/{settings.DB_NAME}"
-    )
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
